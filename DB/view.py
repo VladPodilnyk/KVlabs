6b74638846fad1c7f666e7e0b69aacc1a4fb4ecb
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2016-09-13 21:12:25 +0300

    update for view.py

--- DB/view.py
+++ DB/view.py
@@ -12,4 +12,43 @@ class View:
         for element in lst:
             print '\n'.join(map(lambda key: key + ' : ' + element[key]), element.keys())
             print '\n'
-            
+
+
+    @staticmethod
+    def cinemas_menu():
+        print 'Cinemas menu\n\n'
+        print '1) Add cinema\n \
+               2) Remove cinema\n \
+               3) Update\n \
+               4) Back\n'
+
+    @staticmethod
+    def sessions_menu():
+        print 'Sessions menu\n\n'
+        print '1) Add session\n \
+               2) Remove session\n \
+               3) Update\n \
+               4) Back\n'
+
+    @staticmethod
+    def cinemas_update_menu():
+        print 'Cinema update menu\n\n'
+        print '1) Name\n \
+               2) Street\n \
+               3) Back\n'
+
+    @staticmethod
+    def sessions_update_menu():
+        print 'Session update menu\n\n'
+        print '1) Name\n \
+               2) Time\n \
+               3) Cost\n \
+               4) Available seats\n'
+
+    @staticmethod
+    def success_message(message):
+        print message
+
+    @staticmethod
+    def error_message(message):
+        print 'ERROR: ' + message
