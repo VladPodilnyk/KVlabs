725ee85aa4bc16bffa64841807e5c71e85f076fd
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2016-09-17 21:49:21 +0300

    Update model.py

--- DB/model.py
+++ DB/model.py
@@ -14,3 +14,17 @@ class Model:
         except:
             self.__cinema_lst = []
             self.__session_lst = []
+
+
+    def save(self, file_name):
+        with open(file_name, 'wb') as f:
+            pickle.dump([__cinema_lst, __session_lst], f)
+
+
+    def delete_session(self, key, value):
+        self.__session_lst = filter(lambda element: element['key'] != value, self.__session_lst)
+
+
+
+    #def delete_session(arg):
+        #pass
